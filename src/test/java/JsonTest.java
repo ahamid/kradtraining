50ff10ed69f9aba83c49ad60bfb786431be61bac
Aaron Hamid <aaron.hamid@gmail.com>
2013-02-01 11:00:14 -0500

    use krad dd for spring well-formedness; but krad not loaded in tests yet...

--- src/test/java/JsonTest.java
+++ src/test/java/JsonTest.java
@@ -1,7 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.kuali.rice.krad.spring.JsonApplicationContext;
+import org.kuali.rice.krad.uif.field.InputField;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractRefreshableConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -29,11 +32,46 @@ public class JsonTest {
         Assert.assertEquals("childB", childB.getString());
     }
 
+    private static String[] KRAD_DD = {
+        "classpath:org/kuali/rice/kns/bo/datadictionary/DataDictionaryBaseTypes.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/AdHocRoutePerson.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/AdHocRouteWorkgroup.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/Attachment.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/AttributeReferenceDummy.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/AttributeReferenceElements.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/BusinessObjectAttributeEntry.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/DataDictionaryBaseTypes.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/DocumentHeader.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/Note.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/NoteType.xml",
+        "classpath:org/kuali/rice/krad/bo/datadictionary/PessimisticLock.xml",
+        "classpath:org/kuali/rice/krad/uif/UifActionDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifViewPageDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifHeaderFooterDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifGroupDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifLayoutManagerDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifMaintenanceDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifDocumentDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifInquiryDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifLookupDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifIncidentReportDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifFieldDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifControlDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifWidgetDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifConfigurationDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifRiceDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifElementDefinitions.xml",
+        "classpath:org/kuali/rice/krad/uif/UifInitiateDocumentInfoDefinitions.xml",
+    };
+
     @Test
     public void test_krad_dom() {
-        JsonApplicationContext ac = new JsonApplicationContext();
+        ApplicationContext krad = new ClassPathXmlApplicationContext(KRAD_DD);
+        JsonApplicationContext ac = new JsonApplicationContext(krad);
         ac.setFormat(JsonApplicationContext.JsonFormat.CONCISE);
         ac.setConfigLocation("classpath:org/kuali/rice/krtrain/book/BookEntryViewTest.json");
         ac.refresh();
+
+        //Assert.assertEquals(InputField.class, ac.getBean("averageReview").getClass());
     }
 }
